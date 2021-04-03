cc800ec5c17470a6bdecc8e5b32368c3e3c7452d
iannl <66180087+iannl@users.noreply.github.com>
2021-04-02 19:14:18 -0700

    Delete BirdTest directory

--- BirdTest/js/download.js
+++
@@ -1,9 +0,0 @@
-function download(filename, text, type) {
-    var createDl = document.createElement('a');
-    createDl.setAttribute('href', 'data:'+type+';charset=utf-8,' + encodeURIComponent(text));
-    createDl.setAttribute('download', filename);
-    createDl.style.display = 'none';
-    document.body.appendChild(createDl);
-    createDl.click();
-    document.body.removeChild(createDl);
-}
